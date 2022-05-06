92de28887edc7f9e3c7860abbe2da99bff58ac9b
dbonilla <dbonilla@nexura.com>
2022-05-05 23:17:08 -0500

    first commit

---
+++ src/main/java/com/dayana/market/DayanaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.dayana.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayanaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayanaMarketApplication.class, args);
+	}
+
+}
